85a62632601502bcaf9143da1558c2e032a256b5
Tim <timtran007@gmail.com>
2022-12-27 10:53:55 -0600

    commit tree 0fdc88feff6f81d7f9998654f13f0660cdb7292e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+    
+    def match
+        if self.word.chars.sort != words.chars.sort
+            
+            raise AnagramError
+        end
+    end
+    class AnagramError < StandardError
+    end
+end
